2cc2868923d4464807ecdb48f959c4886c8c0f54
Augusto Rey <augustorps@gmail.com>
2025-03-30 16:52:15 -0300

    front-end base

--- js/app.jsx
+++ js/app.jsx
@@ -1,13 +1,65 @@
 import React from 'react';
 
-console.log('app.jsx');
+export default class App extends React.Component {
+	constructor(props) {
 
-function App() {
+		super(props);
 
-	return (
-		<h1>Hello, React!</h1>
-	);
+		this.state = {
+			cities: [
+				{
+					title: 'Joinville',
+					state: 'SC',
+					country: 'Brazil',
+				},
+				{
+					title: 'San Francisco',
+					state: 'CA',
+					country: 'USA',
+				},
+				{
+					title: 'Urubici',
+					state: 'SC',
+					country: 'Brazil',
+				},
+			],
+		};
 
+	}
+	componentDidMount() {
+
+
+
+	}
+	render() {
+
+		return (
+			<div className="app">
+
+				<header className="header">
+					<h1 className="head-title">Weather</h1>
+					<p className="head-caption">Check the weather in some of the relevant cities for the ProjectMark team.</p>
+				</header>
+
+				<section className="list">
+					{this.state.cities.map((item, i) => {
+
+						return (
+							<article className="item">
+								<h2 className="item-title">{item.title}</h2>
+								<p className="item-location">{item.state}, {item.country}</p>
+							</article>
+						);
+
+					})}
+				</section>
+
+				<footer className="footer">
+					<p>Project developed by Augusto Rey in 2025 for ProjectMark's front-end coding challenge.</p>
+				</footer>
+
+			</div>
+		);
+
+	}
 }
-
-export default App;
